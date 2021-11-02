e29b673934d1ab05d443cbdc94162b78f6fce48d
young <easydev@nate.com>
2021-11-02 14:39:23 +0900

    #1

---
+++ Assignment_01(월_퀴즈_0.25)_1.4-2.0/app.js
@@ -0,0 +1,15 @@
+/*
+모든 브라우저들은 세가지 언어만를 이해함
+1. HTML
+2. CSS
+3. JAVASCRIPT
+
+
+electronjs.org
+html, css, javascript로 데스크톱 앱을 만들 수 있다.
+
+백엔드에도 javascript를 사용할 수  있다.
+
+ml5.js를 쓰면 머신러닝 모델을 생성하는 웹사이트를 구축해서
+그 모델을 훈련시킬 수도 있다.
+*/
